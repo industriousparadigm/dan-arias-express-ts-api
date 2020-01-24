7ff4b6a19f5a2574f36d977fd8a1e812bd89cb08
mrsalvini <dsgmcosta@gmail.com>
2020-01-24 21:08:35 +0000

    item services added, endpoints added, errorhandler added

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -39,3 +39,31 @@ export const find = async (id: number): Promise<Item> => {
 
   throw new Error('No record found')
 }
+
+export const create = async (newItem: Item): Promise<void> => {
+  const id = new Date().valueOf()
+  items[id] = {
+    ...newItem,
+    id
+  }
+}
+
+export const update = async (updatedItem: Item): Promise<void> => {
+  if (items[updatedItem.id]) {
+    items[updatedItem.id] = updatedItem
+    return
+  }
+
+  throw new Error('No record found to update')
+}
+
+export const remove = async (id: number): Promise<void> => {
+  const record: Item = items[id]
+
+  if (record) {
+    delete items[id]
+    return
+  }
+
+  throw new Error('No record found to delete')
+}
